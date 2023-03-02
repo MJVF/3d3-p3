2ab2fd7ec8015559fdbd0a60140842a005fd1065
MJVF <michaeljvflynn@gmail.com>
2023-03-02 15:30:52 +0000

    work on server.c

--- project2/src/server.c
+++ project2/src/server.c
@@ -19,20 +19,20 @@ int main(int argc , char *argv[])
         addr.sin_addr.s_addr = INADDR_ANY; //To avoid binding socket to a specific IP address, better porting from machine to machine
 
     //define socket descriptor o utilise send() and recv(), SOCK_STREAM and not SOCK_DGRAM as we want to connect to client
-    int sock = socket(AF_INET,SOCK_STREAM, 0);
+    int server_sock = socket(AF_INET,SOCK_STREAM, 0);
     //Allow for multiple client connections
     int option = 1;
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&option, sizeof(option));
-    //Bind Socket to PORT
-    bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&option, sizeof(option));
+    //Bind Socket to the specified PORT
+    bind(server_sock, (struct sockaddr *)&addr, sizeof(addr));
     printf("PORT %d Open, ready to connect!\n", atoi(argv[1]));
 
 
 
     //Prepare to trade information with client
     fd_set incoming;
-    int cl_sock;
-    int inc_sock;
+    int client_sock = 0;
+    int incoming_sock = 0;
     char listings[4096];
     int length_addr = sizeof(addr);
 
@@ -41,14 +41,14 @@ int main(int argc , char *argv[])
 
         //Reset socket inbetween different client communications
         FD_ZERO(&incoming);
-        FD_SET(sock, &incoming);
+        FD_SET(server_sock, &incoming);
 
         //define different socked descriptors
-        int max_sd = sock;
-        int new_sd = cl_sock;
-        if(cl_sock > 0)
+        int max_sd = server_sock;
+        int new_sd = client_sock;
+        if(client_sock > 0)
             FD_SET(new_sd, &incoming);
-        if(new_sd > sock)
+        if(new_sd > server_sock)
             max_sd = new_sd;
 
         //Wait for activity on potential sockets, produce error if errno code is not EINTR (i.e failure to select socket)
@@ -57,13 +57,14 @@ int main(int argc , char *argv[])
         }
 
         //If connection is succesful, begin bit stream between client and server
-        if(FD_ISSET(sock, &incoming)){
+        if(FD_ISSET(server_sock, &incoming)){
             
             //Accept the incoming socket request and send message to the client
-            inc_sock = accept(sock, (struct sockaddr *)&addr, (socklen_t*)&length_addr);
-            send(inc_sock, argv[2], strlen(argv[2]), 0);
+            incoming_sock = accept(server_sock, (struct sockaddr *)&addr, (socklen_t*)&length_addr);
+            int valread = read(incoming_sock, listings, 4096);
+            send(incoming_sock, argv[2], strlen(argv[2]), 0);
         
-            cl_sock = inc_sock;
+            new_sd = client_sock;
         
         
         }
